2c1ad3248d98b05e0151830333cd954c9c7e2d68
Oscar Cuellar <odcuellarm@gmail.com>
2021-03-02 12:33:20 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/Main';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+      <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Oscar Mendez</Link>} scroll>
+            <Navigation>
+                <Link to="/">Projects</Link>
+                <Link to="/">Resume</Link>
+                <Link to="/">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Oscar Mendez</Link>}>
+            <Navigation>
+            <Link to="/">Projects</Link>
+              <Link to="/">Resume</Link>
+              <Link to="/">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+      </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+import { Cell, Grid } from 'react-mdl';
+
+class LandingPage extends Component{
+    
+    render(){
+        return(
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                       
+                        <div className="banner-text">
+                            <h1 style={{marginBottom: '2px'}}>Oscar Mendez</h1>
+                            <hr/>
+                            <h3 style={{marginTop: '2px', marginBottom: '2px'}}>Front-End Web Developer</h3>
+                            <div>
+                                <h4 style={{marginTop: '2px'}}>Let's create stunning products together</h4>
+
+                                <div className="social-links"> 
+                                    <div style={{marginRight: '10px'}}>
+                                        <a href="https://www.linkedin.com/" rel="noopener noreferrer" target="_blank">
+                                            <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                                        </a>                                    
+                                    </div>                            
+                                    <div style={{margin: '0 6px'}}>
+                                        <a href="https://github.com/Odcuellarm" rel="noopener noreferrer" target="_blank">
+                                            <i className="fa fa-github-square" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                    <div style={{marginLeft: '10px'}}>
+                                        <a href="https://twitter.com/?lang=en" rel="noopener noreferrer" target="_blank">
+                                            <i className="fa fa-twitter-square" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                    
+                                </div>
+                            </div>                                                
+                                                      
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
