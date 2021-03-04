50a6d22fed2a106e5ae8fc4e7f3de367e4856037
Oscar Cuellar <odcuellarm@gmail.com>
2021-03-03 20:41:55 -0600

    final details

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,46 +7,36 @@ class Contact extends Component{
         return(
            <div className="contact-body">
                <div className="contact-grid">
-                    <div>
+                    <div className='form-container'>
                         <form className="form-horizontal">
-
-                        <div className="form-group">
-                            <label for="exampleFormControlInput1" className="contact-label">Name   </label>
-                            <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleFormControlInput1">Email   </label>
-                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                        </div>
-                                                    
-                        <div className="form-group">
-                            
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Your text goes here"></textarea>
-                        </div>
-                        </form>
+                            <div className="form-group">
+                                <label for="exampleFormControlInput1" className="contact-label">Name   </label>
+                                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleFormControlInput1" className="contact-label">Email   </label>
+                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                            </div>                             
+                            <div className="form-group">
+                                <label for="exampleFormControlInput1" className="contact-label">Message   </label>
+                                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Your text goes here"></textarea>
+                            </div>
+                        </form>  
                     </div>
+                    <div className="personal-info">
+                        <div className='contact-title'>
+                            <h2>Contact Me</h2>
+                            <hr/> 
+                        </div> 
+                        <div className="phone">
+                            <i className="fa fa-phone-square" aria-hidden="true"/>
+                            <h3>(720)435-0645</h3>
+                        </div>                              
                         
-                    
-                    <div>
-                        <h2>Contact Me</h2>
-                        <hr/>
-                        <div className="contact-list">
-                            <List>
-                                <ListItem>
-                                    <ListItemContent style={{fontSize: '26px', fontFamily: 'Roboto'}}>
-                                        <i className="fa fa-phone-square" aria-hidden="true"/>
-                                        (720)435-0645
-                                    </ListItemContent>
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemContent style={{fontSize: '26px', fontFamily: 'Roboto'}}>
-                                        <i className="fa fa-envelope-square" aria-hidden="true"/>
-                                        cmendezoscar@gmail.com
-                                    </ListItemContent>
-                                </ListItem>
-                            </List>
-
-                        </div>
+                        <div className="phone">
+                            <i className="fa fa-envelope-square" aria-hidden="true"/>
+                            <h4>cmendezoscar@gmail.com</h4>
+                        </div> 
                     </div>
                </div>
            </div>
